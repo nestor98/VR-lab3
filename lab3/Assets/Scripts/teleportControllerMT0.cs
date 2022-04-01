41cb0f43c06b8f67ec6e13e65e484b204ae4468e
nestor98 <nestormg98@gmail.com>
2022-04-01 17:35:31 +0200

    TP bien bonito

--- lab3/Assets/Scripts/teleportControllerMT0.cs
+++ lab3/Assets/Scripts/teleportControllerMT0.cs
@@ -2,19 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class teleportControllerMT0 : MonoBehaviour
 {
     // Move in xz with WASD, look around with the mouse.
     // Press SPACE to show/hide the cursor
     // Press 1..4 to choose between the four rooms
-    public enum ControlMode {
-        NORMAL,
-        TELEPORT_INSTANT,
-        TELEPORT_PREVIEW
-    };
-
-    public ControlMode controlMode = ControlMode.TELEPORT_INSTANT;
-
 
     public float moveSpeed = 4.0f;
     public float rotSpeed = 240.0f;
@@ -37,13 +29,6 @@ public class playerController : MonoBehaviour
 
     private Camera cam;
 
-    // Teleport stuff:
-    public string playableTag;
-    public float maxTPRange;
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -56,7 +41,6 @@ public class playerController : MonoBehaviour
     {
         // Movement and looking inputs
         ApplyInputs();
-        //if (controlMode == TELEPORT_PREVIEW) ApplyInputsNormal();
 
         // Handle ball interaction:
         UpdateBall();
@@ -66,99 +50,37 @@ public class playerController : MonoBehaviour
 
         if (Input.GetKeyDown("space")) // Toggle cursor
             Cursor.visible = !Cursor.visible;
-        
+
     }
 
     private void ApplyInputs()
     {
-
         // Rotation input:
         float y = Input.GetAxis("Mouse X"),
-              x = -Input.GetAxis("Mouse Y");
+                x = -Input.GetAxis("Mouse Y");
 
         Vector3 euler = new Vector3(x, y, 0) * rotSpeed * Time.deltaTime;
         transform.eulerAngles += euler;
 
         euler = transform.eulerAngles;
 
-        if (controlMode == ControlMode.NORMAL)
+        // Movement input:
+        Vector3 movement = MovementInputs();
+        if (movement.sqrMagnitude > 1e-6)
         {
-            // Movement input:
-            Vector3 movement = MovementInputs();
-            if (movement.sqrMagnitude > 1e-6)
-            {
-                transform.eulerAngles = new Vector3(0, cam.transform.eulerAngles.y, 0); // Only move in xz plane
-                transform.Translate(movement * moveSpeed * Time.deltaTime);
-            }
-        }
-        else if (controlMode == ControlMode.TELEPORT_INSTANT)
-        {
-            // Movement input:
-            Vector3 position;
-            if (TeleportPosition(out position))
-            {
-                transform.position = position;
-            }
-        }
-        else if (controlMode == ControlMode.TELEPORT_INSTANT)
-        {
-            // Movement input:
-            Vector3 position;
-            if (TeleportPositionPreview(out position))
-            {
-                transform.position = position;
-            }
+            transform.eulerAngles = new Vector3(0, cam.transform.eulerAngles.y, 0); // Only move in xz plane
+            transform.Translate(movement * moveSpeed * Time.deltaTime);
         }
 
         // Restore latitude
         transform.eulerAngles = euler;
     }
-    
+
     // Returns the Vector3 with the movement inputs
     private Vector3 MovementInputs()
     {
         float x = Input.GetAxis("Horizontal"), z = Input.GetAxis("Vertical");
         return new Vector3(x, 0, z);
-    }
-
-    // Returns the Vector3 with the position for the TP
-    private bool TeleportPosition(out Vector3 newPos)
-    {
-        bool moved = Input.GetMouseButtonDown(1);
-        if (moved) {
-            newPos = RaycastPosition();
-        }
-        else newPos = new Vector3();
-        return moved;
-    }
-    // Returns the Vector3 with the position for the TP
-    private bool TeleportPositionPreview(out Vector3 newPos)
-    {
-        bool moved = Input.GetMouseButtonUp(1);
-
-        if (moved)
-        {
-            newPos = RaycastPosition();
-        }
-        else
-        {
-            return false;
-        }
-        return moved;
-    }
-
-    private Vector3 RaycastPosition()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, maxTPRange))
-        {
-            return new Vector3(hit.point.x, transform.position.y, hit.point.z);
-        }
-        else
-        {
-            Vector3 fwd = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
-            return transform.position + fwd * maxTPRange;
-        }
     }
 
     // Triggered by the physics events:
@@ -169,7 +91,7 @@ public class playerController : MonoBehaviour
             cooldown = collision_cd;
             item = obj.gameObject;
             item.transform.position = Camera.main.transform.position
-                                      + Camera.main.transform.forward * 0.75f;
+                                        + Camera.main.transform.forward * 0.75f;
             isHolding = true;
             item.GetComponent<Rigidbody>().useGravity = false;
             item.GetComponent<Rigidbody>().detectCollisions = true;
@@ -198,9 +120,10 @@ public class playerController : MonoBehaviour
                 {
                     currentHeldDownTime = Mathf.Min(currentHeldDownTime + Time.deltaTime, maxForceTime);
                 }
-                else if (Input.GetKeyUp("z")) {
+                else if (Input.GetKeyUp("z"))
+                {
                     float throwForce = minThrowForce + (maxThrowForce - minThrowForce) * currentHeldDownTime / maxForceTime;
-                    print("Current throw force: " +  throwForce);
+                    print("Current throw force: " + throwForce);
                     // Throw
                     var cam = Camera.main;
                     item.GetComponent<Rigidbody>().AddForce(
